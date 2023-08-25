3dbc5af64ffbd6141d6f5de5001d1b494d7f2e3a
Xaviera Sadiya <123913465+XavieraSal@users.noreply.github.com>
2023-08-25 11:21:54 +0700

    Add files via upload
    soal analisis 1 setelah diubah tipe datanya

---
+++ Soal1.java
@@ -0,0 +1,16 @@
+package tes1;
+
+public class Soal1 {
+	public static void main (String[] args) {
+	byte angka1 = 125;
+	byte angka2 = 6;
+	int hasil = (angka1+angka2);
+		
+	System.out.println("Hasil 1 "+hasil);
+	}
+}
+
+//hasil penjumlahan sesuai
+//"(byte)" dihilangkan, kode itu disebut casting
+//Casting digunakan untuk mengubah tipe data ekspresi tertentu menjadi tipe data lain
+// tidak perlu melakukan casting karena hasil penjumlahan langsung disimpan dalam variabel hasil yang bertipe int
